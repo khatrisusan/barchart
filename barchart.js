53643edeef260287ee58e6a8159fde027f280b16
Susan Khatri <khatrisusan>
2020-02-13 21:45:31 +0100

    added dynamic number to last of array and deleted old value

---
+++ barchart.js
@@ -0,0 +1,44 @@
+"use strict"
+ window.addEventListener("DOMContentLoaded", init)
+function init(){
+    console.log("DOMLoaded");
+    arrayExceeds();
+   
+} 
+//make dynamic array that adds 1 each time to last value
+let arr = [];
+let count = 0;
+let timeOut = 1000;
+function arrayExceeds() {
+  if (count <= arr.length) {
+    addSomething();
+  } else {
+    // remove the last no.
+    //still add something
+    addSomething();
+    arr.shift();
+  }
+}
+
+function addSomething() {
+  arr.push(count);
+  count++;
+  console.log(arr);
+  setTimeout(arrayExceeds, timeOut);
+  setHeight();
+}
+
+function setHeight(){
+    for(let i =0; i<=arr.length; i++){
+        let bar;
+        // find the bar
+bar= document.querySelector(`#container > div:nth-child(${i+1})`)
+ //set the height of the bar...
+ bar.style.setProperty("--height", arr[i]);
+ console.log(i);
+ console.log("height")
+    }
+        
+    
+
+}
